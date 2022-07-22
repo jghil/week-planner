c13cc7ae4bebdbcab8ee2373114d0af3cb316ddc
Brennon Toomepuu <btoomepuu@gmail.com>
2022-07-22 22:34:15 +0000

    progress....

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var $entryButton = document.querySelector('.add-entry');
 var $container = document.querySelector('.container');
-
+var $modalContainer = document.querySelector('.modal-container');
 $entryButton.addEventListener('click', openModal);
 
 function openModal(event) {
   $container.className = 'container-clicked';
-
+  $modalContainer.className = 'modal-container';
 }
